218b5cff1df0c8d5b7cee67e03ec33ec0349d31b
Anand Prabhu <anandprabhu530@gmail.com>
2024-04-12 22:32:54 +0530

    Added Boilerplate

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
 export default function Home() {
   return (
     <div>
-      <div>Hello World</div>
+      <div className="border-2 border-black">
+        <div className="w-full flex border-2 border-red-500 justify-between items-center p-4">
+          <div className="font-semibold text-xl">Mirthly Health</div>
+          <div>Tagline Here</div>
+          <div className="flex gap-4">
+            <button className="bg-transparent cursor-pointer p-2">
+              Sign Up
+            </button>
+            <button className="bg-slate-300 rounded-xl py-2 px-4">
+              Log in
+            </button>
+          </div>
+        </div>
+        <div className="w-full flex justify-center items-center">
+          Models here
+        </div>
+      </div>
     </div>
   );
 }
